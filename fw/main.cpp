579afa766bd5a602bf880fc8ddb9c89f90affa1d
Josh Pieper <jjp@pobox.com>
2018-10-26 15:19:38 -0400

    Checkpoint of async_uart
    TX is likely working, although RX can still drop data

--- fw/main.cpp
+++ fw/main.cpp
@@ -1,5 +1,83 @@
 // Copyright 2018 Josh Pieper, jjp@pobox.com.  All rights reserved.
 
+#include <inttypes.h>
+
+#include "mbed.h"
+#include "mbed_events.h"
+
+#include "async_stream.h"
+#include "stm32f466_async_uart.h"
+
+namespace {
+
+static constexpr char kMessage[] = "hello\r\n";
+
+class Emitter {
+ public:
+  Emitter(EventQueue* queue, AsyncStream* stream, DigitalOut* led)
+      : stream_(stream), led_(led) {
+    queue->call_every(1000, this, &Emitter::Emit);
+
+    StartRead();
+  }
+
+  void Emit() {
+    *led_ = !(*led_);
+
+    printf("Emit %" PRIu32 " %d\r\n", count_++, printing_);
+
+    if (printing_) { return; }
+    printing_ = true;
+
+    AsyncWrite(*stream_, string_view::ensure_z(kMessage), [this](ErrorCode ec) {
+        MBED_ASSERT(ec == 0);
+        printf("callback\r\n");
+        printing_ = false;
+      });
+  }
+
+  void StartRead() {
+    stream_->AsyncReadSome(string_span(readbuf_, sizeof(readbuf_) - 1),
+                           [this](ErrorCode ec, size_t amount) {
+                             this->HandleRead(ec, amount);
+                           });
+  }
+
+ private:
+  void HandleRead(ErrorCode ec, size_t amount) {
+    MBED_ASSERT(ec == 0);
+
+    readbuf_[amount] = 0;
+    const int int_amount = static_cast<int>(amount);
+    printf("read %d bytes: '%s'\r\n", int_amount, readbuf_);
+
+    StartRead();
+  }
+
+  AsyncStream* const stream_;
+  DigitalOut* const led_;
+
+  bool printing_ = false;
+  uint32_t count_ = 0;
+
+  char readbuf_[16] = {};
+};
+}
+
 int main(void) {
-  while (1) {}
+  EventQueue queue(4096);
+
+  DigitalOut led(LED1);
+
+  Stm32F466AsyncUart::Options pc_options;
+  pc_options.tx = PC_10;
+  pc_options.rx = PC_11;
+  pc_options.baud_rate = 9600;
+  Stm32F466AsyncUart pc(&queue, pc_options);
+
+  Emitter emitter(&queue, &pc, &led);
+
+  queue.dispatch_forever();
+
+  return 0;
 }
--- fw/stm32f466_async_uart.cpp
+++ fw/stm32f466_async_uart.cpp
@@ -7,13 +7,33 @@
 #include "mbed.h"
 #include "serial_api_hal.h"
 
+#include "error.h"
 #include "irq_callback_table.h"
+
+namespace {
+IRQn_Type FindUartRxIrq(USART_TypeDef* uart) {
+  switch(reinterpret_cast<uint32_t>(uart)) {
+    case UART_1: return USART1_IRQn;
+    case UART_2: return USART2_IRQn;
+    case UART_3: return USART3_IRQn;
+    case UART_4: return UART4_IRQn;
+    case UART_5: return UART5_IRQn;
+    case UART_6: return USART6_IRQn;
+  }
+  MBED_ASSERT(false);
+  return {};
+}
+}
 
 class Stm32F466AsyncUart::Impl : public RawSerial {
  public:
   Impl(EventQueue* event_queue, const Options& options)
       : RawSerial(options.tx, options.rx, options.baud_rate),
         event_queue_(event_queue) {
+    // Just in case no one else has done it yet.
+    __HAL_RCC_DMA1_CLK_ENABLE();
+    __HAL_RCC_DMA2_CLK_ENABLE();
+
     uart_ = [&]() {
       const auto uart_tx = static_cast<UARTName>(
           pinmap_peripheral(options.tx, PinMap_UART_TX));
@@ -22,6 +42,7 @@ class Stm32F466AsyncUart::Impl : public RawSerial {
       return reinterpret_cast<USART_TypeDef*>(pinmap_merge(uart_tx, uart_rx));
     }();
     MBED_ASSERT(uart_ != nullptr);
+    uart_rx_irq_ = FindUartRxIrq(uart_);
 
     // TODO(josh.pieper): For now, we will hard-code which stream to
     // use when there are multiple options.  Perhaps later, the
@@ -39,10 +60,15 @@ class Stm32F466AsyncUart::Impl : public RawSerial {
     // case, or even better, getting it into an appropriate state.  We
     // won't worry about it for now.
 
+    // TODO(jpieper): Configure the FIFO to reduce the bus contention
+    // involved with writing data.
+
     if (options.tx != NC) {
       tx_dma_.stream -> PAR = reinterpret_cast<uint32_t>(&(uart_->DR));
       tx_dma_.stream -> CR =
-          tx_dma_.channel | DMA_SxCR_MINC | DMA_MEMORY_TO_PERIPH |
+          tx_dma_.channel |
+          DMA_SxCR_MINC |
+          DMA_MEMORY_TO_PERIPH |
           DMA_SxCR_TCIE | DMA_SxCR_TEIE;
 
       tx_callback_ = IrqCallbackTable::MakeFunction([this]() {
@@ -58,14 +84,20 @@ class Stm32F466AsyncUart::Impl : public RawSerial {
           rx_dma_.channel | DMA_SxCR_MINC | DMA_PERIPH_TO_MEMORY |
           DMA_SxCR_TCIE | DMA_SxCR_TEIE;
 
-      // Terminate DMA transactions when there is idle time on the bus.
-      uart_->CR1 |= USART_CR1_IDLEIE;
-
       rx_callback_ = IrqCallbackTable::MakeFunction([this]() {
           this->HandleReceive();
         });
       NVIC_SetVector(rx_dma_.irq, reinterpret_cast<uint32_t>(rx_callback_.irq_function));
       NVIC_EnableIRQ(rx_dma_.irq);
+
+      // Terminate DMA transactions when there is idle time on the bus.
+      uart_->CR1 |= USART_CR1_IDLEIE;
+
+      uart_callback_ = IrqCallbackTable::MakeFunction([this]() {
+          this->HandleUart();
+        });
+      NVIC_SetVector(uart_rx_irq_, reinterpret_cast<uint32_t>(uart_callback_.irq_function));
+      NVIC_EnableIRQ(uart_rx_irq_);
     }
   }
 
@@ -89,6 +121,8 @@ class Stm32F466AsyncUart::Impl : public RawSerial {
     rx_dma_.stream->CR |= DMA_SxCR_EN;
 
     uart_  -> CR3 |= USART_CR3_DMAR;
+
+    printf("AsyncReadSome size=%d\r\n", data.size());
   }
 
   void AsyncWriteSome(const string_view& data, const SizeCallback& callback) {
@@ -118,13 +152,19 @@ class Stm32F466AsyncUart::Impl : public RawSerial {
     const size_t amount_sent = tx_size_ - tx_dma_.stream->NDTR;
     int error_code = 0;
 
+    // The enable bit should be 0 at this point.
+    MBED_ASSERT((tx_dma_.stream->CR & DMA_SxCR_EN) == 0);
+
+    // Tell the UART to stop requesting DMA.
+    uart_->CR3 &= ~(USART_CR3_DMAT);
+
     if (*tx_dma_.status_register & tx_dma_.status_teif) {
       // We've got an error, report it.
       *tx_dma_.status_clear |= tx_dma_.status_teif;
-      error_code = 1;
+      error_code = kDmaStreamTransferError;
     } else if (*tx_dma_.status_register & tx_dma_.status_feif) {
       *tx_dma_.status_clear |= tx_dma_.status_feif;
-      error_code = 2;
+      error_code = kDmaStreamFifoError;
     } else  if (*tx_dma_.status_register & tx_dma_.status_tcif) {
       // Transmit is complete.
       *tx_dma_.status_clear |= tx_dma_.status_tcif;
@@ -133,14 +173,16 @@ class Stm32F466AsyncUart::Impl : public RawSerial {
       MBED_ASSERT(false);
     }
 
-    event_queue_->call(this, &Impl::EventHandleTransmit, error_code, amount_sent);
+    const int id = event_queue_->call(this, &Impl::EventHandleTransmit, error_code, amount_sent);
+    MBED_ASSERT(id != 0);
 
     // TODO(jpieper): Verify that USART_CR3_DMAT gets cleared here on
     // its own even if we send back to back quickly.
   }
 
   void EventHandleTransmit(int error_code, size_t amount_sent) {
-    event_queue_->call(current_write_callback_.shrink<4>(), error_code, amount_sent);
+    const int id = event_queue_->call(current_write_callback_, error_code, amount_sent);
+    MBED_ASSERT(id != 0);
     current_write_callback_ = {};
   }
 
@@ -153,17 +195,17 @@ class Stm32F466AsyncUart::Impl : public RawSerial {
       *rx_dma_.status_clear |= rx_dma_.status_teif;
       const auto uart_sr = uart_->SR;
       if (uart_sr & USART_SR_ORE) {
-        error_code = 3;
+        error_code = kUartOverrunError;
       } else if (uart_sr & USART_SR_FE) {
-        error_code = 4;
+        error_code = kUartFramingError;
       } else if (uart_sr & USART_SR_NE) {
-        error_code = 5;
+        error_code = kUartNoiseError;
       } else {
-        error_code = 1;
+        error_code = kDmaStreamTransferError;
       }
     } else if (*rx_dma_.status_register & rx_dma_.status_feif) {
       *rx_dma_.status_clear |= rx_dma_.status_feif;
-      error_code = 2;
+      error_code = kDmaStreamFifoError;
     } else if (*rx_dma_.status_register & rx_dma_.status_tcif) {
       *rx_dma_.status_clear |= rx_dma_.status_tcif;
       error_code = 0;
@@ -171,12 +213,30 @@ class Stm32F466AsyncUart::Impl : public RawSerial {
       MBED_ASSERT(false);
     }
 
-    event_queue_->call(this, &Impl::EventHandleReceive, error_code, amount_received);
+    uart_  -> CR3 &= ~(USART_CR3_DMAR);
+
+    const int id = event_queue_->call(this, &Impl::EventHandleReceive, error_code, amount_received);
+    MBED_ASSERT(id != 0);
   }
 
   void EventHandleReceive(int error_code, size_t amount_received) {
-    event_queue_->call(current_read_callback_.shrink<4>(), error_code, amount_received);
+    const int id = event_queue_->call(current_read_callback_, error_code, amount_received);
+    MBED_ASSERT(id != 0);
     current_read_callback_ = {};
+  }
+
+  // INVOKED FROM INTERRUPT CONTEXT
+  void HandleUart() {
+    if (uart_->SR && USART_FLAG_IDLE) {
+      // Clear the IDLE flag by reading status register, then data register.
+      volatile uint32_t tmp;
+      tmp = uart_->SR;
+      tmp = uart_->DR;
+      (void)tmp;
+
+      // Disable any outstanding DMA transfers.
+      rx_dma_.stream->CR &= ~DMA_SxCR_EN;
+    }
   }
 
   struct Dma {
@@ -229,12 +289,14 @@ class Stm32F466AsyncUart::Impl : public RawSerial {
 
   events::EventQueue* const event_queue_;
   USART_TypeDef* uart_ = nullptr;
+  IRQn_Type uart_rx_irq_ = {};
 
   Dma tx_dma_;
   Dma rx_dma_;
 
   IrqCallbackTable::Callback tx_callback_;
   IrqCallbackTable::Callback rx_callback_;
+  IrqCallbackTable::Callback uart_callback_;
 
   SizeCallback current_read_callback_;
   size_t rx_size_ = 0;
