2a3ebc0e77629eba6bd053e78e4e053441bdbb4f
Josh Pieper <jjp@pobox.com>
2018-11-16 13:49:03 -0500

    Integrate tokenizer

---
+++ mjlib/base/test/tokenizer_test.cc
@@ -0,0 +1,54 @@
+// Copyright 2018 Josh Pieper, jjp@pobox.com.  All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "mjlib/base/tokenizer.h"
+
+#include <boost/test/auto_unit_test.hpp>
+
+using namespace mjlib::base;
+
+BOOST_AUTO_TEST_CASE(BasicTokenizer) {
+  static constexpr const char* kToTokenize = "stuff that I want to send";
+  static constexpr const char* kDelimeters = " ";
+
+  Tokenizer dut(kToTokenize, kDelimeters);
+  BOOST_TEST(dut.remaining() == std::string_view(kToTokenize));
+
+  {
+    const auto first = dut.next();
+    BOOST_TEST(first == std::string_view("stuff"));
+    BOOST_TEST(dut.remaining() == std::string_view("that I want to send"));
+  }
+
+  {
+    const auto next = dut.next();
+    BOOST_TEST(next == std::string_view("that"));
+    BOOST_TEST(dut.remaining() == std::string_view("I want to send"));
+  }
+
+  dut.next();
+  dut.next();
+  dut.next();
+
+  {
+    const auto last = dut.next();
+    BOOST_TEST(last == std::string_view("send"));
+    BOOST_TEST(dut.remaining() == std::string_view(""));
+  }
+
+  {
+    const auto past_end = dut.next();
+    BOOST_TEST(past_end == std::string_view());
+  }
+}
---
+++ mjlib/base/tokenizer.h
@@ -0,0 +1,62 @@
+// Copyright 2015-2018 Josh Pieper, jjp@pobox.com.  All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <cstring>
+#include <string_view>
+
+namespace mjlib {
+namespace base {
+
+/// A simple gsl::string_span based tokenizer.  It can split on
+/// multiple delimiters, and reports multiple consecutive delimiters
+/// as empty tokens.
+class Tokenizer {
+ public:
+  Tokenizer(const std::string_view& source, const char* delimiters)
+      : source_(source),
+        delimiters_(delimiters),
+        position_(source_.cbegin()) {}
+
+  std::string_view next() {
+    if (position_ == source_.end()) { return std::string_view(); }
+
+    const auto start = position_;
+    auto next = position_;
+    bool found = false;
+    for (; next != source_.end(); ++next) {
+      if (std::strchr(delimiters_, *next) != nullptr) {
+        position_ = next;
+        ++position_;
+        found = true;
+        break;
+      }
+    }
+    if (!found) { position_ = next; }
+    return std::string_view(start, next - start);
+  }
+
+  std::string_view remaining() const {
+    return std::string_view(position_, source_.end() - position_);
+  }
+
+ private:
+  const std::string_view source_;
+  const char* const delimiters_;
+  std::string_view::const_iterator position_;
+};
+
+}
+}
