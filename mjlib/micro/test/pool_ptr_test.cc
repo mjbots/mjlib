7d38d3e25165526442291ab372ad92f1ba0a965e
Josh Pieper <jjp@pobox.com>
2018-11-16 13:37:23 -0500

    Integrate PoolPtr

---
+++ mjlib/micro/test/pool_ptr_test.cc
@@ -0,0 +1,44 @@
+// Copyright 2018 Josh Pieper, jjp@pobox.com.  All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "mjlib/micro/pool_ptr.h"
+
+#include <boost/test/auto_unit_test.hpp>
+
+using namespace mjlib::micro;
+
+namespace {
+struct HasConstructor {
+  HasConstructor(int item1, int item2)
+      : a(item1 + item2),
+        b(item1 - item2) {}
+
+  int a = 0;
+  int b = 1;
+};
+}
+
+BOOST_AUTO_TEST_CASE(BasicPoolPtr) {
+  SizedPool<> pool;
+
+  PoolPtr<int> int_ptr(&pool);
+
+  BOOST_TEST(*int_ptr == 0);
+  *int_ptr = 100;
+  BOOST_TEST(*int_ptr == 100);
+
+  PoolPtr<HasConstructor> hc_ptr(&pool, 10, 20);
+  BOOST_TEST(hc_ptr->a == 30);
+  BOOST_TEST(hc_ptr->b == -10);
+}
