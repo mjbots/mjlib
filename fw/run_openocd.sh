99f561601884b249db4adcc27b751862863700b2
Josh Pieper <jjp@pobox.com>
2018-11-19 10:33:05 -0500

    Remove mbed-cli based build environment

--- fw/run_openocd.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-openocd -f /usr/share/openocd/scripts/board/st_nucleo_f4.cfg -f /usr/share/openocd/scripts/interface/stlink-v2-1.cfg -c init -c "reset_config none separate; reset init"
