d2e10d00d6077c2f5c7bf07235f6cc216b362e36
Josh Pieper <jjp@pobox.com>
2018-11-22 09:15:37 -0500

    Port the PID controller from the gimbal

---
+++ mjlib/base/test/pid_test.cc
@@ -0,0 +1,45 @@
+// Copyright 2015-2018 Josh Pieper, jjp@pobox.com.  All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "mjlib/base/pid.h"
+
+#include <boost/test/auto_unit_test.hpp>
+
+using namespace mjlib::base;
+
+BOOST_AUTO_TEST_CASE(BasicPid) {
+  PID::Config config;
+
+  config.kp = 1.0f;
+  config.ki = 2.0f;
+  config.kd = 3.0f;
+  config.ilimit = 10.0f;
+
+  PID::State state;
+
+  PID dut{&config, &state};
+
+  BOOST_TEST(state.integral == 0.0f);
+
+  const float result = dut.Apply(1.0f, 3.0f,
+                                 2.0f, 5.0f,
+                                 100.0f);
+  BOOST_TEST(result == -11.04f);
+  BOOST_TEST(state.error == -2.0f);
+  BOOST_TEST(state.error_rate == -3.0f);
+  BOOST_TEST(state.p == -2.0f);
+  BOOST_TEST(state.d == -9.0f);
+  BOOST_TEST(state.pd == -11.0f);
+  BOOST_TEST(state.command == result);
+}
