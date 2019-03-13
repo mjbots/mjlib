e074e7b0c4ccefd31414aed478c045d4cfdcae7f
Josh Pieper <jjp@pobox.com>
2019-03-13 11:03:28 -0400

    Integrate async_stream and async_types

---
+++ mjlib/io/async_stream.h
@@ -0,0 +1,53 @@
+// Copyright 2015-2019 Josh Pieper, jjp@pobox.com.  All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <functional>
+
+#include <boost/asio/io_service.hpp>
+#include <boost/noncopyable.hpp>
+
+#include "mjlib/io/async_types.h"
+
+namespace mjlib {
+namespace io {
+
+class AsyncReadStream : boost::noncopyable {
+ public:
+  virtual ~AsyncReadStream() {}
+
+  virtual void AsyncReadSome(MutableBufferSequence, ReadHandler) = 0;
+};
+
+class AsyncWriteStream : boost::noncopyable {
+ public:
+  virtual ~AsyncWriteStream() {}
+
+  virtual void AsyncWriteSome(ConstBufferSequence, WriteHandler) = 0;
+};
+
+class AsyncStream : public AsyncReadStream, public AsyncWriteStream {
+ public:
+  ~AsyncStream() override {}
+
+  virtual boost::asio::io_service& get_io_service() = 0;
+  virtual void cancel() = 0;
+};
+
+using SharedStream = std::shared_ptr<AsyncStream>;
+using StreamHandler = std::function<void (const base::error_code&, SharedStream)>;
+
+}
+}
--- mjlib/io/async_types.h
+++ mjlib/io/async_types.h
@@ -1,4 +1,4 @@
-// Copyright 2015-2016 Josh Pieper, jjp@pobox.com.  All rights reserved.
+// Copyright 2015-2019 Josh Pieper, jjp@pobox.com.  All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,14 +15,62 @@
 #pragma once
 
 #include <functional>
+#include <vector>
 
-#include "error_code.h"
+#include <boost/asio/buffer.hpp>
 
-namespace mjmech {
-namespace base {
+#include "mjlib/base/error_code.h"
 
-typedef std::function<void ()> NullHandler;
-typedef std::function<void (ErrorCode)> ErrorHandler;
+namespace mjlib {
+namespace io {
+
+// Concrete types that model various callbacks, including concepts
+// from boost::asio.
+using VoidCallback = std::function<void ()>;
+using ErrorCallback = std::function<void (const base::error_code&)>;
+using SizeCallback = std::function<void (const base::error_code&, ssize_t)>;
+
+using ReadHandler = SizeCallback;
+using WriteHandler = SizeCallback;
+
+// And concrete types that model the various boost::asio buffer
+// types.
+template <typename Buffer>
+class BufferSequence {
+ public:
+  typedef Buffer value_type;
+  typedef typename std::vector<value_type>::const_iterator const_iterator;
+
+  BufferSequence() {}
+
+  template <typename Sequence>
+  BufferSequence(const Sequence& s) : data_(s.begin(), s.end()) {}
+
+  const_iterator begin() const { return data_.begin(); }
+  const_iterator end() const { return data_.end(); }
+
+ protected:
+  std::vector<value_type> data_;
+};
+
+class MutableBufferSequence
+    : public BufferSequence<boost::asio::mutable_buffer> {
+ public:
+  MutableBufferSequence() {}
+
+  template <typename Buffers>
+  MutableBufferSequence(const Buffers& t)
+  : BufferSequence<boost::asio::mutable_buffer>(t) {}
+};
+
+class ConstBufferSequence : public BufferSequence<boost::asio::const_buffer> {
+ public:
+  ConstBufferSequence() {}
+
+  template <typename Buffers>
+  ConstBufferSequence(const Buffers& t)
+  : BufferSequence<boost::asio::const_buffer>(t) {}
+};
 
 }
 }
