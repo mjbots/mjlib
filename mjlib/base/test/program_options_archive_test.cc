e4104622cc0c030604622b23526080b097810297
Josh Pieper <jjp@pobox.com>
2019-03-13 11:57:34 -0400

    Port fail and program_options_archive from mjmech

---
+++ mjlib/base/test/program_options_archive_test.cc
@@ -0,0 +1,51 @@
+// Copyright 2019 Josh Pieper, jjp@pobox.com.  All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "mjlib/base/program_options_archive.h"
+
+#include <sstream>
+
+#include <boost/test/auto_unit_test.hpp>
+
+namespace {
+struct MyStruct {
+  int int_value = 1;
+  std::string string_value = "2";
+
+  template <typename Archive>
+  void Serialize(Archive* a) {
+    a->Visit(MJ_NVP(int_value));
+    a->Visit(MJ_NVP(string_value));
+  }
+};
+
+std::string StripSpaces(const std::string& in) {
+  std::stringstream result;
+  for (char c : in) {
+    if (c != ' ') { result.write(&c, 1); }
+  }
+  return result.str();
+}
+}
+
+BOOST_AUTO_TEST_CASE(ProgramOptionsArchiveTest) {
+  MyStruct my_struct;
+  boost::program_options::options_description desc;
+  mjlib::base::ProgramOptionsArchive(&desc).Accept(&my_struct);
+
+  std::stringstream ss;
+  ss << desc;
+  std::string no_spaces = StripSpaces(ss.str());
+  BOOST_TEST(no_spaces == "--int_value\n--string_value\n");
+}
