b90d7ffcfc81635a6281b8cf4ad2570db082b778
Josh Pieper <jjp@pobox.com>
2019-03-23 09:42:29 -0400

    asio_client: Implement register operations

--- mjlib/multiplex/asio_client.cc
+++ mjlib/multiplex/asio_client.cc
@@ -14,7 +14,19 @@
 
 #include "mjlib/multiplex/asio_client.h"
 
+#include <functional>
+
+#include <boost/asio/write.hpp>
+#include <boost/crc.hpp>
+
+#include "mjlib/base/fail.h"
+#include "mjlib/base/fast_stream.h"
 #include "mjlib/io/exclusive_command.h"
+
+#include "mjlib/multiplex/frame_stream.h"
+#include "mjlib/multiplex/stream.h"
+
+namespace pl = std::placeholders;
 
 namespace mjlib {
 namespace multiplex {
@@ -23,30 +35,65 @@ class AsioClient::Impl {
  public:
   Impl(io::AsyncStream* stream, const Options& options)
       : stream_(stream),
-        options_(options) {}
+        options_(options),
+        frame_stream_(stream_) {}
 
   void AsyncRegister(uint8_t id,
                      const RegisterRequest& request,
                      RegisterHandler handler) {
-    lock_.Invoke([id, request](RegisterHandler handler) {
-        // Create our full frame.
+    lock_.Invoke([this, id, request](RegisterHandler handler) {
+        tx_frame_.source_id = this->options_.source_id;
+        tx_frame_.dest_id = id;
+        tx_frame_.request_reply = request.request_reply();
+        tx_frame_.payload = request.buffer();
 
-        // Send it.
-
-        // Read a reply.
-        handler(base::error_code(), RegisterReply());
+        frame_stream_.AsyncWrite(
+            &tx_frame_, std::bind(&Impl::HandleWrite, this, pl::_1,
+                                  handler, request.request_reply()));
       },
       handler);
+  }
+
+  void HandleWrite(const base::error_code& ec,
+                   RegisterHandler handler,
+                   bool request_reply) {
+    if (!request_reply) {
+      stream_->get_io_service().post(std::bind(handler, ec, RegisterReply()));
+      return;
+    }
+
+    FailIf(ec);
+
+    const boost::posix_time::time_duration kDefaultTimeout =
+        boost::posix_time::milliseconds(10);
+
+    frame_stream_.AsyncRead(
+        &rx_frame_, kDefaultTimeout,
+        std::bind(&Impl::HandleRead, this, pl::_1, handler));
+  }
+
+  void HandleRead(const base::error_code& ec, RegisterHandler handler) {
+    base::FailIf(ec);
+
+    base::FastIStringStream stream(rx_frame_.payload);
+    auto reply = ParseRegisterReply(stream);
+    stream_->get_io_service().post(
+        std::bind(handler, ec, reply));
   }
 
   io::SharedStream MakeTunnel(uint8_t, uint32_t, const TunnelOptions&) {
     return {};
   }
 
+ private:
   io::AsyncStream* const stream_;
   const Options options_;
+  FrameStream frame_stream_;
 
   io::ExclusiveCommand lock_{stream_->get_io_service()};
+
+  Frame rx_frame_;
+  Frame tx_frame_;
 };
 
 AsioClient::AsioClient(io::AsyncStream* stream, const Options& options)
---
+++ mjlib/multiplex/test/asio_client_test.cc
@@ -0,0 +1,106 @@
+// Copyright 2019 Josh Pieper, jjp@pobox.com.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "mjlib/multiplex/asio_client.h"
+
+#include <boost/asio/write.hpp>
+#include <boost/test/auto_unit_test.hpp>
+
+#include "mjlib/io/debug_deadline_service.h"
+#include "mjlib/io/stream_pipe_factory.h"
+#include "mjlib/io/test/reader.h"
+
+namespace base = mjlib::base;
+namespace io = mjlib::io;
+namespace mp = mjlib::multiplex;
+using mp::AsioClient;
+
+namespace {
+struct Fixture {
+  void Poll() {
+    service.poll();
+    service.reset();
+  }
+
+  void AsyncRegister(const mp::RegisterRequest& request) {
+    dut.AsyncRegister(2, request, [this](const base::error_code& ec,
+                                         const mp::RegisterReply& reply_in) {
+                        mjlib::base::FailIf(ec);
+                        this->register_done++;
+                        this->reply = reply_in;
+                      });
+  }
+
+  boost::asio::io_service service;
+  io::DebugDeadlineService* const debug_service{
+    io::DebugDeadlineService::Install(service)};
+  io::StreamPipeFactory pipe_factory{service};
+  io::SharedStream client_side{pipe_factory.GetStream("", 1)};
+  AsioClient dut{client_side.get()};
+
+  io::SharedStream server_side{pipe_factory.GetStream("", 0)};
+  io::test::Reader server_reader{server_side.get()};
+
+  int register_done = 0;
+  mp::RegisterReply reply;
+};
+}
+
+BOOST_FIXTURE_TEST_CASE(AsioClientRegisterNoReply, Fixture) {
+  mp::RegisterRequest request;
+  request.WriteSingle(1, static_cast<int8_t>(10));
+
+  AsyncRegister(request);
+
+  BOOST_TEST(register_done == 0);
+
+  Poll();
+
+  BOOST_TEST(register_done == 1);
+  BOOST_TEST(reply.size() == 0);
+  BOOST_TEST(server_reader.data().size() == 10u);
+}
+
+BOOST_FIXTURE_TEST_CASE(AsioClientRegisterReply, Fixture) {
+  mp::RegisterRequest request;
+  request.ReadSingle(3, 0);
+
+  AsyncRegister(request);
+
+  BOOST_TEST(register_done == 0);
+  Poll();
+
+  BOOST_TEST(register_done == 0);
+  BOOST_TEST(server_reader.data().size() == 9u);
+
+  // Write the device's reply.
+  int write_done = 0;
+  boost::asio::async_write(
+      *server_side,
+      boost::asio::buffer("\x54\xab\x02\x00\x03\x20\x03\x04\x9a\xf8", 10),
+      [&](auto&& ec, size_t) {
+        base::FailIf(ec);
+        write_done++;
+      });
+
+  BOOST_TEST(write_done == 0);
+
+  Poll();
+  BOOST_TEST(write_done == 1);
+  BOOST_TEST(register_done == 1);
+  BOOST_TEST(reply.size() == 1);
+  BOOST_TEST(reply.count(3) == 1);
+  BOOST_TEST((reply.at(3) == mp::Format::ReadResult(
+                  mp::Format::Value(static_cast<int8_t>(4)))));
+}
