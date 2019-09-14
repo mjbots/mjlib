58a2c56e5cc36f58057a043555cec91f2bfb4ed9
Josh Pieper <jjp@pobox.com>
2019-09-14 10:52:50 -0400

    First implementation of an archive to emit JSON5

---
+++ mjlib/base/json5_write_archive.h
@@ -0,0 +1,251 @@
+// Copyright 2019 Josh Pieper, jjp@pobox.com.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <array>
+#include <iostream>
+#include <optional>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+#include <fmt/format.h>
+
+#include "mjlib/base/bytes.h"
+#include "mjlib/base/priority_tag.h"
+#include "mjlib/base/visitor.h"
+#include "mjlib/base/visit_archive.h"
+
+namespace mjlib {
+namespace base {
+
+/// Emit JSON5 from a given serializable.
+///
+/// https://json5.org
+class Json5WriteArchive : public VisitArchive<Json5WriteArchive> {
+ public:
+  struct Options {
+    int indent = 0;
+
+    Options() {}
+
+    Options& set_indent(int value) {
+      indent = value;
+      return *this;
+    }
+  };
+
+  Json5WriteArchive(std::ostream& stream, const Options& options = Options())
+      : stream_(stream),
+        options_(options) {}
+
+  template <typename Value>
+  static std::string Write(const Value& value) {
+    std::ostringstream ostr;
+    Json5WriteArchive(ostr).Value(value);
+    return ostr.str();
+  }
+
+  template <typename Serializable>
+  Json5WriteArchive& Accept(Serializable* serializable) {
+    stream_ << "{\n";
+    VisitArchive<Json5WriteArchive>::Accept(serializable);
+    stream_ << Indent(-2) << "}";
+    return *this;
+  }
+
+  template <typename ValueType>
+  Json5WriteArchive& Value(const ValueType& value) {
+    base::ReferenceNameValuePair nvp(const_cast<ValueType*>(&value), "");
+    VisitArchive<Json5WriteArchive>::Visit(nvp);
+    return *this;
+  }
+
+  template <typename NameValuePair>
+  void Visit(const NameValuePair& nvp) {
+    // Write out the field name and colon.
+    stream_ << Indent() << "\"" << nvp.name() << "\" : ";
+    Value(nvp.get_value());
+
+    stream_ << ",\n";
+  }
+
+  template <typename NameValuePair>
+  void VisitSerializable(const NameValuePair& nvp) {
+    auto new_options = options_;
+    Json5WriteArchive sub_archive(
+        stream_, new_options.set_indent(options_.indent + 2));
+    sub_archive.Accept(nvp.value());
+  }
+
+  template <typename NameValuePair>
+  void VisitEnumeration(const NameValuePair& nvp) {
+    stream_ << "\"" << nvp.enumeration_mapper()[*nvp.value()] << "\"";
+  }
+
+  template <typename NameValuePair>
+  void VisitArray(const NameValuePair& nvp) {
+    WriteArray(*nvp.value());
+  }
+
+  template <typename NameValuePair>
+  void VisitScalar(const NameValuePair& nvp) {
+    VisitHelper(nvp, nvp.value(), base::PriorityTag<2>());
+  }
+
+ private:
+  template <typename NameValuePair>
+  void VisitHelper(const NameValuePair& nvp,
+                   Bytes* value,
+                   base::PriorityTag<2> tag) {
+    VisitHelper(nvp, static_cast<std::vector<uint8_t>*>(value), tag);
+  }
+
+  template <typename NameValuePair, typename T>
+  void VisitHelper(const NameValuePair&,
+                   std::vector<T>* value,
+                   base::PriorityTag<1>) {
+    WriteArray(*value);
+  }
+
+  template <typename NameValuePair, typename T>
+  void VisitHelper(const NameValuePair&,
+                   std::optional<T>* value,
+                   base::PriorityTag<1>) {
+    if (!(*value)) {
+      stream_ << "null";
+    } else {
+      Value(**value);
+    }
+  }
+
+  template <typename NameValuePair>
+  void VisitHelper(const NameValuePair&,
+                   boost::posix_time::ptime* value,
+                   base::PriorityTag<1>) {
+    stream_ << "\"";
+    stream_ << boost::posix_time::to_simple_string(*value);
+    stream_ << "\"";
+  }
+
+  template <typename NameValuePair>
+  void VisitHelper(const NameValuePair&,
+                   boost::posix_time::time_duration* value,
+                   base::PriorityTag<1>) {
+    stream_ << "\"";
+    stream_ << boost::posix_time::to_simple_string(*value);
+    stream_ << "\"";
+  }
+
+  template <typename NameValuePair>
+  void VisitHelper(const NameValuePair&,
+                   std::string* value,
+                   base::PriorityTag<1>) {
+    stream_ << "\"";
+    stream_ << EscapeString(*value);
+    stream_ << "\"";
+  }
+
+  template <typename NameValuePair>
+  void VisitHelper(const NameValuePair&,
+                   float* value,
+                   base::PriorityTag<1>) {
+    stream_ << fmt::format("{:.9g}", *value);
+  }
+
+  template <typename NameValuePair>
+  void VisitHelper(const NameValuePair&,
+                   double* value,
+                   base::PriorityTag<1>) {
+    stream_ << fmt::format("{:.17g}", *value);
+  }
+
+  template <typename NameValuePair, typename T>
+  void VisitHelper(const NameValuePair&,
+                   T* value,
+                   base::PriorityTag<0>) {
+    stream_ << fmt::format("{:d}", *value);
+  }
+
+  template <typename Array>
+  void WriteArray(Array& array) {
+    stream_ << "[\n";
+    auto options_copy = options_;
+    Json5WriteArchive sub_archive(
+        stream_, options_copy.set_indent(options_copy.indent + 2));
+    for (auto& item : array) {
+      stream_ << Indent(2);
+      Value(item);
+      stream_ << ",\n";
+    }
+    stream_ << Indent() << "]";
+  }
+
+  std::string EscapeString(const std::string& in) {
+    std::ostringstream out;
+    for (char c : in) {
+      switch (c) {
+        case '"': {
+          out << "\\\"";
+          break;
+        }
+        case '\\': {
+          out << "\\\\";
+          break;
+        }
+        case '\b': {
+          out << "\\b";
+          break;
+        }
+        case '\f': {
+          out << "\\f";
+          break;
+        }
+        case '\n': {
+          out << "\\n";
+          break;
+        }
+        case '\r': {
+          out << "\\r";
+          break;
+        }
+        case '\t': {
+          out << "\\t";
+          break;
+        }
+        case 0: {
+          out << "\\u0000";
+          break;
+        }
+        default: {
+          out << c;
+          break;
+        }
+      }
+    }
+
+    return out.str();
+  }
+
+  std::string Indent(int extra = 0) {
+    return std::string(options_.indent + extra + 2, ' ');
+  }
+
+  std::ostream& stream_;
+  const Options options_;
+};
+
+}
+}
---
+++ mjlib/base/test/json5_write_archive_test.cc
@@ -0,0 +1,72 @@
+// Copyright 2019 Josh Pieper, jjp@pobox.com.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "mjlib/base/json5_write_archive.h"
+
+#include <sstream>
+
+#include <boost/test/auto_unit_test.hpp>
+
+#include "mjlib/base/test/all_types_struct.h"
+
+using namespace mjlib;
+
+BOOST_AUTO_TEST_CASE(BasicJson5Write) {
+  std::ostringstream ostr;
+  base::test::AllTypesTest all_types;
+  base::Json5WriteArchive(ostr).Accept(&all_types);
+
+  const std::string actual = ostr.str();
+  const std::string expected = R"XXX({
+  "value_bool" : 0,
+  "value_i8" : -1,
+  "value_i16" : -2,
+  "value_i32" : -3,
+  "value_i64" : -4,
+  "value_u8" : 5,
+  "value_u16" : 6,
+  "value_u32" : 7,
+  "value_u64" : 8,
+  "value_f32" : 9,
+  "value_f64" : 10,
+  "value_bytes" : [
+    11,
+    12,
+    13,
+  ],
+  "value_str" : "de",
+  "value_object" : {
+    "value_u32" : 3,
+  },
+  "value_enum" : 0,
+  "value_array" : [
+    {
+    "value_u32" : 3,
+  },
+  ],
+  "value_optional" : 21,
+  "value_timestamp" : "1970-Jan-01 00:00:01",
+  "value_duration" : "00:00:00.500000",
+})XXX";
+  BOOST_TEST(actual == expected);
+}
+
+BOOST_AUTO_TEST_CASE(JsonStringEscape) {
+  BOOST_TEST(base::Json5WriteArchive::Write(std::string("abcdef"))
+             == "\"abcdef\"");
+  BOOST_TEST(base::Json5WriteArchive::Write(
+                 std::string("a\"\\\b\f\n\r\t\x00"
+                             "def", 12))
+             == "\"a\\\"\\\\\\b\\f\\n\\r\\t\\u0000def\"");
+}
