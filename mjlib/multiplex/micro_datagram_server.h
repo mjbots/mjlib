3f395897dd3eccaf4d8be4e5ff5bbf4949ae82b8
Josh Pieper <jjp@pobox.com>
2019-12-06 13:13:56 -0500

    Stub of a separate class to decouple multiplex framing

---
+++ mjlib/multiplex/micro_datagram_server.h
@@ -0,0 +1,44 @@
+// Copyright 2019 Josh Pieper, jjp@pobox.com.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <string_view>
+
+#include "mjlib/base/string_span.h"
+
+#include "mjlib/micro/async_types.h"
+
+namespace mjlib {
+namespace multiplex {
+
+class MicroDatagramServer {
+ public:
+  virtual ~MicroDatagramServer() {}
+
+  struct Header {
+    int source = 0;
+    int destination = 0;
+    int size = 0;
+  };
+
+  virtual void AsyncRead(Header&, const base::string_span&,
+                         const micro::SizeCallback&) = 0;
+
+  virtual void AsyncWrite(const Header&, const std::string_view&,
+                          const micro::SizeCallback&) = 0;
+};
+
+}
+}
---
+++ mjlib/multiplex/micro_stream_datagram.h
@@ -0,0 +1,45 @@
+// Copyright 2019 Josh Pieper, jjp@pobox.com.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include "mjlib/micro/async_stream.h"
+#include "mjlib/micro/pool_ptr.h"
+
+#include "mjlib/multiplex/micro_datagram_server.h"
+
+namespace mjlib {
+namespace multiplex {
+
+class MicroStreamDatagram : public MicroDatagramServer {
+ public:
+  struct Options {
+    size_t buffer_size = 256;
+  };
+
+  MicroStreamDatagram(micro::Pool*, micro::AsyncStream*, const Options&);
+  ~MicroStreamDatagram();
+
+  void AsyncRead(Header&, const base::string_span&,
+                 const micro::SizeCallback&) override;
+  void AsyncWrite(const Header&, const std::string_view&,
+                  const micro::SizeCallback&) override;
+
+ private:
+  class Impl;
+  micro::PoolPtr<Impl> impl_;
+};
+
+}
+}
