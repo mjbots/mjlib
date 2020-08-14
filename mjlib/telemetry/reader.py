161657fba7e4d4dd1492341f6465678930b10636
Josh Pieper <jjp@pobox.com>
2020-08-14 08:04:34 -0400

    Update the enum _missing_ to work with python 3.7 and greater

--- mjlib/telemetry/reader.py
+++ mjlib/telemetry/reader.py
@@ -323,8 +323,18 @@ class EnumType:
 
         class Enum(enum.IntEnum):
             @classmethod
-            def _missing_(cls, name):
-                return name
+            def _missing_(cls, value):
+                return cls._create_pseudo_member_(value)
+
+            @classmethod
+            def _create_pseudo_member_(cls, value):
+                pseudo_member = cls._value2member_map_.get(value, None)
+                if pseudo_member is None:
+                    new_member = int.__new__(cls, value)
+                    new_member._name_ = str(value)
+                    new_member._value_ = value
+                    pseudo_member = cls._value2member_map_.setdefault(value, new_member)
+                return pseudo_member
 
         self.enum_class = Enum(name, items)
 
