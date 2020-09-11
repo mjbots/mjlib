8f34a017cfbdacbc4ab583095c6cd66c75da262a
Josh Pieper <jjp@pobox.com>
2020-09-11 11:02:15 -0400

    Attempt to pip install snappy in travis

--- travis-ci.sh
+++ travis-ci.sh
@@ -2,5 +2,10 @@
 
 set -ev
 
+python3 -c "import snappy"
+if [ $? -ne 0 ]; then
+    pip3 install snappy
+fi
+
 ./tools/bazel test --copt -Werror //...
 ./tools/bazel build --copt -Werror --copt -Wdouble-promotion --cpu=stm32f4 -c opt //:target
