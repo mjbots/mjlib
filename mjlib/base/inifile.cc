1429dec43ae3fd4ef415d32aa74d0ec4009dfa87
Josh Pieper <jjp@pobox.com>
2020-10-25 22:32:13 -0400

    Add missing header include

--- mjlib/base/inifile.cc
+++ mjlib/base/inifile.cc
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include "mjlib/base/inifile.h"
+
 #include <fmt/format.h>
 
 #include <boost/algorithm/string.hpp>
